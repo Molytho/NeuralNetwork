5bf4865d88dbbab19ddd11bac1b86bfd83f1dcc2
Robin Ebert <ebertrobin2002@gmail.com>
2020-10-19 14:08:36 +0200

    Added basic interfaces

---
+++ NeuralNetwork/INeuralLayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Molytho.Matrix;
+
+namespace Molytho.NeuralNetwork
+{
+    public interface INeuralLayer
+    {
+        public Vector<float> Calculate(Vector<float> input);
+        public Func<Vector<float>, Vector<float>> ActivationFunction { get; }
+        public int InputSize { get; }
+        public int OutputSize { get; }
+        public INeuralLayer? NextLayer { get; set; }
+    }
+}
---
+++ NeuralNetwork/ITrainableNeuralLayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Molytho.Matrix;
+
+namespace Molytho.NeuralNetwork
+{
+    public interface ITrainableNeuralLayer : INeuralLayer
+    {
+        public Matrix<float> Weights { get; }
+        public Vector<float> LastInput { get; }
+        public Vector<float> LastOutput { get; }
+        public Func<Vector<float>, Vector<float>> DifferentialActivationFunction { get; }
+    }
+}
